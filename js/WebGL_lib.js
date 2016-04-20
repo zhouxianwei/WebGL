e7a1d69607f06a008b97074a7f91b3eea7f243d8
zhouxianwei <banbanlaohu@sina.com>
2016-04-20 15:11:01 +0800

    first commit

---
+++ js/WebGL_lib.js
@@ -0,0 +1,31 @@
+function getWebGLContext(canvas) {
+    // body...  
+    var WebGL = canvas.getContext("experimental-webgl");
+
+    return WebGL;
+}
+
+function initShaders(webgl, VS_SOURCE, FS_SOURCE) {
+    var VS = webgl.createShader(webgl.VERTEX_SHADER);
+    var FS = webgl.createShader(webgl.FRAGMENT_SHADER);
+    webgl.shaderSource(VS, VSHADER_SOURCE);
+    webgl.shaderSource(FS, FSHADER_SOURCE);
+    webgl.compileShader(VS);
+    if (!webgl.getShaderParameter(VS, webgl.COMPILE_STATUS)) {
+        alert("An error occurred compiling the shaders: " + webgl.getShaderInfoLog(VS));
+        return null;
+    }
+    webgl.compileShader(FS);
+    if (!webgl.getShaderParameter(FS, webgl.COMPILE_STATUS)) {
+        alert("An error occurred compiling the shaders: " + webgl.getShaderInfoLog(FS));
+        return null;
+    }
+    var shaderProgram = webgl.createProgram();
+    webgl.attachShader(shaderProgram, VS);
+    webgl.attachShader(shaderProgram, FS);
+    webgl.linkProgram(shaderProgram);
+    webgl.useProgram(shaderProgram);
+    if (!webgl.getProgramParameter(shaderProgram, webgl.LINK_STATUS)) {
+        alert("Unable to initialize the shader program.");
+    }
+}
